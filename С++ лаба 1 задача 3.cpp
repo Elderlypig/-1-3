2635c131f919461f6b3962ab87b74c3a4a0e724c
Elderly Pig <Elderly Pig@ELDERLYPIG>
2023-11-14 20:49:11 +0300

    Добавьте файлы проекта.

---
+++ С++ лаба 1 задача 3.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+int getProductOfDigits(int number) {
+    int product = 1;
+    while (number != 0) {
+        int digit = number % 10;
+        product *= digit;
+        number /= 10;
+    }
+    return product;
+}
+
+int main() {
+    int m, n;
+    setlocale(LC_ALL, "Russian"); 
+
+    // Ввод значений m и n
+    std::cout << "Введите значение m: ";
+    std::cin >> m;
+    std::cout << "Введите значение n: ";
+    std::cin >> n;
+
+    int k = m + 1;
+    while (getProductOfDigits(k) <= n) {
+        k++;
+    }
+
+    std::cout << "k = " << k << std::endl;
+
+    return 0;
+}
+
